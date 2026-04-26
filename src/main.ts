d8139629aaa2efdec1955a3045a87f746455e1ba
miquel febrer <miquelfebrercapo@gmail.com>
2026-04-26 14:45:26 +0200

    se cambia a microservicios

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('ProductsApp');
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    }
+  );
+
+  // app.setGlobalPrefix('api/v1');
+
+
   app.useGlobalPipes(new ValidationPipe(
     {
       whitelist: true, // Elimina propiedades no definidas en el DTO
@@ -14,7 +26,7 @@ async function bootstrap() {
       transform: true, // Transforma los payloads a los tipos definidos en los DTOs
     }
   ));
-  await app.listen(envs.port);
-  logger.log(`Server is running on port ${envs.port}`);
+  await app.listen();
+  logger.log(`Products Microservice is running on port ${envs.port}`);
 }
 bootstrap();
